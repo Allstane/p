dfde106d916e5670cbc8e5e669e90ebf8355be82
Stanislav Kapinus <st.kapinus@yandex.ru>
2023-09-26 09:25:24 +0300

    restBooks

--- src/main/scala/world/library/templates/BooksT.scala
+++ src/main/scala/world/library/templates/BooksT.scala
@@ -4,11 +4,19 @@ import world.library.data.{Book, Chapter, MetabookF}
 
 case class BooksT(leftBook: Book, rightBook: Book, metabookF: MetabookF, l: Chapter, r: Chapter)
   extends Template {
+
+  def restBooks(mF: MetabookF, isLeft: Boolean, bid: Int): String =
+    s"<select name='books' id='$isLeft'>" +
+      mF.books.filter(_.id != bid).map(b => s"<option value='${b.id}'>${b.title}</option>").mkString +
+      "</select>"
+
   val content: String = {
     val table: String = s"<p align='center'> <table width='800' height='100%'> " +
-      s"<tr> <td> ${leftBook.title} </td> <td> ${rightBook.title} </td> </tr>"
+      s"<tr align='center'> <td width='45%'> ${restBooks(metabookF, isLeft = true, leftBook.id)} </td> " +
+      s"<td width='45%'> ${restBooks(metabookF, isLeft = false, rightBook.id)} </td> </tr>" +
+      s"<tr align='center'> <td width='45%'> ${ leftBook.title} </td> <td width='45%'> ${rightBook.title} </td> </tr>"
     val chapters: String = s"<tr> <td>${l.title}</td> <td>${r.title}</td>  </tr>" +
-      s"<tr valign='top'> <td align='justify'>${l.txt.getOrElse("")}</td> <td align='justify'>${r.txt.getOrElse("")}</td>  </tr>"
+      s"<tr valign='top'> <td align='justify'>${l.txt.getOrElse("")}</td> <td align='justify'>${r.txt.getOrElse("")}</td> </tr>"
     table + chapters + "</table></p>"
   }
 
