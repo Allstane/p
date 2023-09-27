533d7bcc1c51e1e390e07432a68e7ff4fadbce5f
Stanislav <142887480+stakap@users.noreply.github.com>
2023-09-27 10:06:39 +0300

    8-chapter-view (#7)
    * start
    
    * restBooks
    
    * restBooks

--- src/main/scala/world/library/templates/Template.scala
+++ src/main/scala/world/library/templates/Template.scala
@@ -8,7 +8,7 @@ trait Template {
        |<meta http-equiv="Content-Type" content="text/html; charset="UTF-8">
        |<title>Alefowl.com: bilingual books</title></head>
        |<body>
-       |<header align='center'> Alefowl.com: bilingual books </p>
+       |<header align='center'><a href='/'>Alefowl.com: bilingual books</a></p>
        |$content
        |<p align='center'> </p>
        |<footer>About Vacancies Policies <a href='/auth'>Login</a> <a href='/regform'>Registration</a></footer>
